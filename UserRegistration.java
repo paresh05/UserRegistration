b08a5b8a36185347039c6b6559a69c87e36d323d
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-09-06 15:17:58 +0530

    Added: Welcome message

---
+++ UserRegistration.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.bridgelabzday22;
+
+/**
+ * @author paresh.praveen_ymedi
+ *
+ */
+public class UserRegistration {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to User Registration Program");
+
+	}
+
+}
