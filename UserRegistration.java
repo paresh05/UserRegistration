de0811f20e468c0d52b55b4d4ae116a0f8df7a2c
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-09-06 18:00:58 +0530

    Added: Method for first name

--- UserRegistration.java
+++ UserRegistration.java
@@ -3,11 +3,35 @@
  */
 package com.bridgelabzday22;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author paresh.praveen_ymedi
  *
  */
 public class UserRegistration {
+	
+	public static void firstName() {
+		
+		String firstNameRegex="^[A-Z].{2,}" ;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the First Name");
+		String firstName=sc.nextLine();
+		
+		Pattern pt = Pattern.compile(firstNameRegex);
+		Matcher mt = pt.matcher(firstName);
+		
+		boolean result = mt.matches();
+		
+		if(result)
+			System.out.println("Valid First Name");
+		else
+			System.out.println("Invalid First Name");
+		
+		sc.close();
+	}
 
 	/**
 	 * @param args
@@ -15,7 +39,8 @@ public class UserRegistration {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to User Registration Program");
-
+		firstName();
+		
 	}
 
 }
