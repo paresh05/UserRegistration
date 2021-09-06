baf406b500fffcb5365b6b754e2545fcfa8e9900
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-09-06 23:12:24 +0530

    Refactored: Password Method to check for atleast 1 numeric number

--- UserRegistration.java
+++ UserRegistration.java
@@ -117,12 +117,13 @@ public class UserRegistration {
 	 * This Function checks and prints if the Password entered is valid or not
 	 * The rule-1 for a valid password is it should have minimum of 8 characters
 	 * The rule-2 for a valid password is it should have at least 1 upper case character
+	 * The rule-3 for a valid password is it should have at least 1 numeric number
 	 * @param password This parameter takes the string input from the user 
 	 */
 	
 	public static void password() {
 
-		String passwordRegex = "((?=.*[A-Z]).{8,})";
+		String passwordRegex = "((?=.*\\d)(?=.*[A-Z]).{8,})";
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the Password");
 		String password = sc.nextLine();
