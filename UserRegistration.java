f99b4eb74ba45926ad9521c73bff2908701d8f6e
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-09-06 18:25:40 +0530

    Added: Method for last name

--- UserRegistration.java
+++ UserRegistration.java
@@ -12,35 +12,64 @@ import java.util.regex.Pattern;
  *
  */
 public class UserRegistration {
-	
+
+	/**
+	 * This Function checks and prints if the first name entered is valid or not
+	 * @param firtsName This parameter takes the string input from the user 
+	 */
 	public static void firstName() {
-		
-		String firstNameRegex="^[A-Z].{2,}" ;
+
+		String firstNameRegex = "^[A-Z].{2,}";
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the First Name");
-		String firstName=sc.nextLine();
-		
+		String firstName = sc.nextLine();
+
 		Pattern pt = Pattern.compile(firstNameRegex);
 		Matcher mt = pt.matcher(firstName);
-		
+
 		boolean result = mt.matches();
-		
-		if(result)
+
+		if (result)
 			System.out.println("Valid First Name");
 		else
 			System.out.println("Invalid First Name");
-		
+
+	}
+	
+	/**
+	 * This Function checks and prints if the last name entered is valid or not
+	 * @param lastName This parameter takes the string input from the user 
+	 */
+	
+	public static void lastName() {
+
+		String lastNameRegex = "^[A-Z].{2,}";
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Last Name");
+		String lastName = sc.nextLine();
+
+		Pattern pt = Pattern.compile(lastNameRegex);
+		Matcher mt = pt.matcher(lastName);
+
+		boolean result = mt.matches();
+
+		if (result)
+			System.out.println("Valid Last Name");
+		else
+			System.out.println("Invalid Last Name");
+
 		sc.close();
 	}
-
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 		System.out.println("Welcome to User Registration Program");
 		firstName();
-		
+		lastName();
+
 	}
 
 }
