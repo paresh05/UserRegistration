bc2fec5926a87b217a5d4e8eeb73f299f9035581
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-09-12 18:21:58 +0530

    Refactor: To throw custom exception when invalid input is given

--- UserRegistration.java
+++ UserRegistration.java
@@ -15,8 +15,9 @@ public class UserRegistration {
 	/**
 	 * This Function checks and prints if the first name entered is valid or not
 	 * @param firtsName This parameter takes the string input from the user 
+	 * @throws UserRegistrationException 
 	 */
-	public static boolean firstName(String firstName) {
+	public static boolean firstName(String firstName) throws UserRegistrationException {
 
 		String firstNameRegex = "^[A-Z]{1}[a-z]{2,}";
 		
@@ -24,6 +25,11 @@ public class UserRegistration {
 		Matcher mt = pt.matcher(firstName);
 
 		boolean result = mt.matches();
+		
+		if(result == false)
+		{
+			throw new UserRegistrationException("Please Enter Valid First Name");
+		}
 
 		return result;
 	}
@@ -31,9 +37,10 @@ public class UserRegistration {
 	/**
 	 * This Function checks and prints if the last name entered is valid or not
 	 * @param lastName This parameter takes the string input from the user 
+	 * @throws UserRegistrationException 
 	 */
 	
-	public static boolean lastName(String lastName) {
+	public static boolean lastName(String lastName) throws UserRegistrationException {
 
 		String lastNameRegex = "^[A-Z]{1}[a-z]{2,}";
 
@@ -41,7 +48,12 @@ public class UserRegistration {
 		Matcher mt = pt.matcher(lastName);
 
 		boolean result = mt.matches();
-
+		
+		if(result == false)
+		{
+			throw new UserRegistrationException("Please Enter Valid Last Name");
+		}
+		
 		return result;
 	}
 	
@@ -49,9 +61,10 @@ public class UserRegistration {
 	/**
 	 * This Function checks and prints if the email entered is valid or not
 	 * @param email This parameter takes the string input from the user 
+	 * @throws UserRegistrationException 
 	 */
 	
-	public static boolean email(String email) {
+	public static boolean email(String email) throws UserRegistrationException {
 
 		String emailRegex = "[a-z0-9]+(.[a-z0-9]+)@[a-z0-9]+.[a-z]*.[a-z]*$";
 		
@@ -59,7 +72,12 @@ public class UserRegistration {
 		Matcher mt = pt.matcher(email);
 
 		boolean result = mt.matches();
-
+		
+		if(result == false)
+		{
+			throw new UserRegistrationException("Please Enter Valid E-mail");
+		}
+		
 		return result;
 	}
 	
@@ -67,9 +85,10 @@ public class UserRegistration {
 	/**
 	 * This Function checks and prints if the Mobile Number entered is valid or not
 	 * @param mobileNumber This parameter takes the string input from the user 
+	 * @throws UserRegistrationException 
 	 */
 	
-	public static boolean mobileNumber(String mobileNumber) {
+	public static boolean mobileNumber(String mobileNumber) throws UserRegistrationException {
 
 		String mobileNumberRegex = "^[0-9]{2}\s[0-9]{10}";
 
@@ -77,6 +96,11 @@ public class UserRegistration {
 		Matcher mt = pt.matcher(mobileNumber);
 
 		boolean result = mt.matches();
+		
+		if(result == false)
+		{
+			throw new UserRegistrationException("Please Enter Valid Mobile Number");
+		}
 		
 		return result;
 	}
@@ -88,9 +112,10 @@ public class UserRegistration {
 	 * The rule-3 for a valid password is it should have at least 1 numeric number
 	 * The rule-4 for a valid password is it should have at least 1 special character
 	 * @param password This parameter takes the string input from the user 
+	 * @throws UserRegistrationException 
 	 */
 	
-	public static boolean password(String password) {
+	public static boolean password(String password) throws UserRegistrationException {
 
 		String passwordRegex = "((?=.*\\d)(?=.*[A-Z])(?=.*[$&+,:;=?@#|'<>.^*()%!-]).{8,})";
 
@@ -98,17 +123,13 @@ public class UserRegistration {
 		Matcher mt = pt.matcher(password);
 
 		boolean result = mt.matches();
-
-		return result;
-	}
-	
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-
-		System.out.println("Welcome to User Registration Program");
 		
+		if(result == false)
+		{
+			throw new UserRegistrationException("Please Enter Valid Password");
+		}
+		
+		return result;
 	}
 
 }
